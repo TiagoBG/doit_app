f4fac3eaf58760b7c520bbe9f0d734f7c98039bd
Santiago Betancur Graciano <santybg_94@hotmail.com>
2021-02-15 20:11:41 -0500

    Nodemailer successfully working Feb 15 7:25pm

--- frontend/src/components/base/Header.jsx
+++ frontend/src/components/base/Header.jsx
@@ -9,7 +9,7 @@ export default function Header (){
                 <img src={logo} height='75px' alt="DoIt Logo" className='ml-3 my-auto'/>
                 <h1 className='ml-2 my-auto'>DoIt App</h1>
                 <div className="d-flex justify-content-end">
-                    <h4 className='ml-5 mt-4 '>Control your activities</h4>
+                    <h4 className='ml-5 mt-4 '>Track your activities</h4>
                 </div>
             </div>
         </nav>
